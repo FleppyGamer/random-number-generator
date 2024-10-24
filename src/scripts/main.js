51b5db1c9c7753bb4b198be14df03e32d1c8091a
Fleppy_Gamer <fleppyg@gmail.com>
2024-10-24 19:34:38 -0300

    Unnecessary console.log command removed from main.js and this was revised for possible mistypes and errors.

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,9 +4,6 @@ document.addEventListener("DOMContentLoaded", function(){
         e.preventDefault();
 
         let limNum = document.getElementById("lim-num").value;
-
-        console.log(limNum);
-
         limNum = parseInt(limNum);
 
         let randomNum = Math.random() * limNum;
